ed82d739a2582d006a1dcc9b76c933aaa53ac47d
bushuevasofia <bushuevasofia@mail.ru>
2025-06-07 15:12:33 -0400

    add changed

--- cmake/HunterGate.cmake
+++ cmake/HunterGate.cmake
@@ -541,3 +541,4 @@ macro(HunterGate)
     set_property(GLOBAL PROPERTY HUNTER_GATE_DONE YES)
   endif()
 endmacro()
+
---
+++ src/print.cpp
@@ -0,0 +1,11 @@
+#include <print.h>
+
+void print(const std::string& text, std::ostream& out)
+{
+  out << text;
+}
+
+void print(const std::string& text, std::ofstream& out)
+{
+  out << text;
+}
